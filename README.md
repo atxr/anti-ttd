2b4d8bcce547ded65ed2b15d0929739438c29b89
atxr <alexandre.tullot@protonmail.com>
2023-07-13 15:24:11 +0200

    Add ParentProcessName detection.

---
+++ README.md
@@ -0,0 +1,4 @@
+# anti-ttd
+Different method to detect (or not) TTD.exe.
+
+- ParentProcessName: checks if the parent process name is "ttd.exe"
---
+++ anti-ttd/ParentProcessName.cpp
@@ -0,0 +1,69 @@
+#include <Windows.h>
+#include <tlhelp32.h>
+#include <Psapi.h>
+#include <string>
+
+#define MAX_PATH 256
+
+DWORD GetParentProcessId() {
+	DWORD dwCurrentProcessId = GetCurrentProcessId();
+	DWORD dwParentProcessId = -1;
+
+	PROCESSENTRY32 ProcessEntry = { 0 };
+	ProcessEntry.dwSize = sizeof(PROCESSENTRY32);
+
+	HANDLE hSnapShot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, dwCurrentProcessId);
+	if (Process32First(hSnapShot, &ProcessEntry)) {
+		do {
+			if ((ProcessEntry.th32ProcessID == dwCurrentProcessId)) {
+				dwParentProcessId = ProcessEntry.th32ParentProcessID;
+			}
+		} while (Process32Next(hSnapShot, &ProcessEntry));
+	}
+
+	if (hSnapShot) {
+		CloseHandle(hSnapShot);
+	}
+
+	return dwParentProcessId;
+}
+
+bool DetectTTDWithParentProcessName()
+{
+	DWORD dwParentProcessId = GetParentProcessId();
+	if (-1 == dwParentProcessId)
+		return FALSE;
+
+	HANDLE Handle = OpenProcess(
+		PROCESS_QUERY_INFORMATION | PROCESS_VM_READ,
+		FALSE,
+		dwParentProcessId
+	);
+
+	if (!Handle) {
+		wprintf(L"Cannot open process error\n");
+		return FALSE;
+	}
+
+	TCHAR wsWindowTitle[MAX_PATH];
+	int res = GetModuleFileNameEx(Handle, 0, wsWindowTitle, MAX_PATH);
+	CloseHandle(Handle);
+
+	if (!res) {
+		wprintf(L"Cannot get module filename error\n");
+		return FALSE;
+	}
+
+	_wcslwr_s(wsWindowTitle);
+	return wcsstr(wsWindowTitle, L"ttd.exe");
+}
+
+int main() {
+	if (DetectTTDWithParentProcessName()) {
+		printf("TTD detected\n");
+	}
+
+	printf("Hello, World!\n");
+	getchar();
+	return 0;
+}
